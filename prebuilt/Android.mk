bc3fd4bb8d5b554647e1af1ea9c63e2eee9f8930
hmz007 <hmz007@gmail.com>
2016-07-07 15:55:18 +0800

    apps: Support for FriendlyARM iTest
    Signed-off-by: hmz007 <hmz007@gmail.com>

---
+++ prebuilt/Android.mk
@@ -0,0 +1,43 @@
+#
+# Copyright 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_BOARD_PLATFORM),slsiap)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libfriendlyarm-hardware
+LOCAL_MODULE_OWNER := friendlyarm
+LOCAL_SRC_FILES := libfriendlyarm-hardware.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := iTest
+LOCAL_MODULE_PATH := $(TARGET_OUT_APPS_PRIVILEGED)
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_OWNER := friendlyarm
+LOCAL_MODULE_TAGS := optional
+LOCAL_BUILT_MODULE_STEM := package.apk
+LOCAL_SRC_FILES := iTest.apk
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
+
+endif
